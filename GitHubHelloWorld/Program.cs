34c10c1c3b5e4042e78ec72994f7cf51461ad112
Pap Sogome <ssogome@gmail.com>
2017-06-16 10:59:41 -0600

    Add project files.

---
+++ GitHubHelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The world is listening...Press [Enter] key to continue.");
+
+            Console.ReadLine();
+        }
+    }
+}
